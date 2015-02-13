bd278bbd202739895848f7983c097c0a1f988e5a
frankcash <cashc@acm.org>
2015-02-12 22:01:47 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject bookclub "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [compojure "1.3.1"]]
+  :main ^:skip-aot bookclub.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/bookclub/core.clj
@@ -0,0 +1,7 @@
+(ns bookclub.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
