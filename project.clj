613227fe3cc93f951ff19cdd2aa1b5963b9a1286
Bryan Garza <brygarza@gmail.com>
2015-02-13 22:50:54 -0800

    Instead of using jetty directly, pass the handler to lein, lein will then start the server for us. Use middleware provided by ring-defaults, and add a couple of catch-all routes.

--- project.clj
+++ project.clj
@@ -8,8 +8,11 @@
                  [ring/ring-jetty-adapter "1.3.2"]
                  [compojure "1.3.1"]
                  [org.clojure/java.jdbc "0.3.2"]
-                 [postgresql "9.1-901.jdbc4"]]
-  :main ^:skip-aot bookclub.core
-  :plugins [[lein-ring "0.7.1"]]
+                 [postgresql "9.1-901.jdbc4"]
+                 [ring/ring-defaults "0.1.4"]]
+  :plugins [[lein-ring "0.9.1"]]
+  :ring {:handler bookclub.core/app}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}})
